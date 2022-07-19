0e6dccb7528edc0ee40b6068089e47dacd312b51
Ruan Pablo <ruan_goio10@hotmail.com>
2022-07-19 18:04:47 -0300

    Home Screen

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { THEME } from "./src/styles/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 import { Loading } from "./src/Components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -15,7 +16,7 @@ export default function App() {
 
 	return (
 		<NativeBaseProvider theme={THEME}>
-			{fontsLoaded ? <SignIn /> : <Loading/>}
+			{fontsLoaded ? <Home /> : <Loading />}
 		</NativeBaseProvider>
 	);
 }
